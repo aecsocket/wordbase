0293f723e5f1751790bf44f82ba73c55b7ab0370
Matthieu C. <886074+teto@users.noreply.github.com>
2025-06-11 19:05:04 +0200

    adding crane

--- flake.nix
+++ flake.nix
@@ -6,12 +6,14 @@
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    crane.url = "github:ipetkov/crane";
   };
   outputs =
     {
       nixpkgs,
       flake-utils,
       rust-overlay,
+      crane,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
@@ -20,8 +22,19 @@
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
         rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+        craneLib = crane.mkLib pkgs;
       in
       {
+        packages.default = craneLib.buildPackage {
+          src = craneLib.cleanCargoSource ./.;
+
+          # Add extra inputs here or any other derivation settings
+          # doCheck = true;
+          # buildInputs = [];
+          # nativeBuildInputs = [];
+        };
+
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             just
